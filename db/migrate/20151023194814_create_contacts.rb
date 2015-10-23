94c8bced8fad937ab0cd292259910320fb0fabcc
Joshua Reyes <joshua.reyes@ryerson.ca>
2015-10-23 21:11:49 +0000

    IDK something

--- db/migrate/20151023194814_create_contacts.rb
+++ db/migrate/20151023194814_create_contacts.rb
@@ -2,9 +2,8 @@ class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
       t.string :name
-      t. string :email
+      t.string :email
       t.text :comments
-      
       t.timestamps
     end
   end
