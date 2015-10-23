1a754d1be79a6e00a9ac39c2658bb6c04ae78238
Joshua Reyes <joshua.reyes@ryerson.ca>
2015-10-23 20:45:05 +0000

    Added contacts Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :contacts #generating out a few other routes for us a few other urls for us can see by hitting save
-  #typing bundle exec rake routes
-  ##routes file exists so that we can create all the different urls have to define every url for max contorl
-  
+  resources :contacts 
   get '/about' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
